f41d1625d62b7ae8845e36ed5654298f773b3502
Cyrill Martin <cyrill.martin@unibas.ch>
2020-06-19 16:06:09 +0200

    add gui route

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # microservice-json-task
 
-This microservice renders a view to let a user work with JSON data in Python.
+This microservice renders a view to let a user work with JSON data in Python 3 and is part of the microservice pipline in [inseri](https://github.com/nie-ine/inseri). The service provides the possibility to transform a JSON data response received by an API call into the needed data structure in order to pass it to an according inseri app. 
 
-## Dependencies
+The service has two routes: 
+
+1. /json-task
+   The route /json-task is meant for pipelining requests and returns json to be consumed by the next inseri app.
+1. /json-gui
+   The route /json-gui returns the output of the Python code inside the HTML view. 
+
+## Run and Develop Locally
+
+### Dependencies
 1. python3
 2. packages: see requirements.txt
 
-## Install and run
+### Install and Run
 1. ``pip3 install -r requirements.txt``
-2. start with ``python3 json-task.py``
-3. Go to http://localhost:8080/json-task
+1. Run with ``python3 json-task.py``
+1. Go to http://localhost:8080/json-task
+
+## Run with Docker
+
+1. Build the image: ``[sudo] docker build -t nieine/json-task .``
+1. Run the container: ``[sudo] docker run -p 8080:8080 nieine/json-task``
+1. Go to http://localhost:8080/json-task
 
 ## Publish on Dockerhub
 See [microservice-template](https://github.com/nie-ine/microservice-template)
--- json-task.py
+++ json-task.py
@@ -3,7 +3,7 @@ import glob
 import socket
 import subprocess
 import requests, json
-from random import random
+import random
 from flask_cors import CORS
 from flask import Flask, request, render_template
 from flask import jsonify
@@ -11,28 +11,30 @@ from flask import jsonify
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/json-task", methods=["POST","GET"])
+@app.route("/json-task", endpoint="task", methods=["POST","GET"])
+@app.route("/json-gui", endpoint="gui", methods=["POST","GET"])
 def jsontask():
-    # print("here", request.data)
     # POST
     if request.method == "POST":
         if not os.path.exists("temp_files"):
             os.makedirs("temp_files")
 
+        # Going to add a random number for filenames - avoiding any conflicts
+        # temp_id = random.randint(1000000,2000000)
+
         # Get data and temporarily save as file
         d_name = request.form["d_name"]
         data = request.form["data"]
         if not d_name == "":
-            df = open("temp_files/%s" % d_name, "w+")
+            df = open("temp_files/{}".format(d_name), "w+")
             df.write(data)
             df.close()
 
         # Get code and temporarily save as file
         c_name = request.form["c_name"]
-        suffix = ".py"
         code = request.form["code"]
-        if c_name.endswith(suffix) and len(c_name) > 3:
-            cf = open("temp_files/%s" % c_name, "w+")
+        if c_name.endswith(".py") and len(c_name) > 3:
+            cf = open("temp_files/{}".format(c_name), "w+")
             cf.write(code)
             cf.close()
         else:
@@ -42,7 +44,6 @@ def jsontask():
                 data=data,
                 c_name=c_name,
                 code=code)
-
 
         # Enter temp_files directory and execute code file
         os.chdir("temp_files")
@@ -57,16 +58,18 @@ def jsontask():
         except subprocess.CalledProcessError as e:
             # Leave temp_files directory
             os.chdir("..")
-            # Return error message
-            # return render_template("json-task.html",
-            #     output=e.output,
-            #     d_name=d_name,
-            #     data=data,
-            #     c_name=c_name,
-            #     code=code)
-            return jsonify(
-                output=e.output
-            )
+            if request.endpoint == "gui": 
+                # Return error message
+                return render_template("json-task.html",
+                    output=e.output,
+                    d_name=d_name,
+                    data=data,
+                    c_name=c_name,
+                    code=code)
+            else:
+                return jsonify(
+                    output=e.output
+                )
         else:
             # If run successfully, delete the temporarily saved files
             files = glob.glob("*")
@@ -74,19 +77,22 @@ def jsontask():
                 os.remove(f)
             # Leave temp_files directory
             os.chdir("..")
-            # Return output
-            # return render_template("json-task.html",
-            #     output=process,
-            #     d_name=d_name,
-            #     data=data,
-            #     c_name=c_name,
-            #     code=code)
-            return jsonify(
-                output=process
-            )
+            if request.endpoint == "gui":
+                # Return output
+                return render_template("json-task.html",
+                    output=process,
+                    d_name=d_name,
+                    data=data,
+                    c_name=c_name,
+                    code=code)
+            else:
+                return jsonify(
+                    output=process
+                )
     # GET
     else:
-        return render_template("json-task.html")
+        #return "{}".format(request.endpoint)
+        return render_template("json-task.html", endpoint=request.endpoint)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
